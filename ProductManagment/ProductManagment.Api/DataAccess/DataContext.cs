eb473a4c603ec489b1061ac5a15c208c32827f5e
Bartosz Bąk <bartosz.bk04@gmail.com>
2020-05-01 13:49:14 +0200

    Added DataContext

---
+++ ProductManagment/ProductManagment.Api/DataAccess/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductManagment.Api.Models;
+
+namespace ProductManagment.Api.DataAccess
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        DbSet<Product> Products { get; set; }
+        DbSet<Category> Categories { get; set; }
+    }
+}
