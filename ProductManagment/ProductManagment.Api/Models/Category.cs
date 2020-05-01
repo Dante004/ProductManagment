26194d725665c085d1f050b665914c8ec483ac0f
Bartosz Bąk <bartosz.bk04@gmail.com>
2020-05-01 13:48:55 +0200

    Added Models

---
+++ ProductManagment/ProductManagment.Api/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace ProductManagment.Api.Models
+{
+    public class Category : BaseModel
+    {
+        public string Name { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
---
+++ ProductManagment/ProductManagment.Api/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductManagment.Api.Models
+{
+    public class Product : BaseModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
