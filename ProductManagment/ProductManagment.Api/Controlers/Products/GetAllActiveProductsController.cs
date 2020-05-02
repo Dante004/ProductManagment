1914d65853c7813bb115199d8d5eb710a0d6617c
Bartosz Bąk <bartosz.bk04@gmail.com>
2020-05-02 11:26:53 +0200

    Deleted useless namespaces

--- ProductManagment/ProductManagment.Api/Controlers/Products/GetAllActiveProductsController.cs
+++ ProductManagment/ProductManagment.Api/Controlers/Products/GetAllActiveProductsController.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
 using MediatR;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProductManagment.Api.DataAccess;
