150926794a30fa74f2fdc3da524f9ff92c2cd340
Bartosz Bąk <bartosz.bk04@gmail.com>
2020-05-02 12:05:04 +0200

    Added AutoFac module

---
+++ ProductManagment/ProductManagment.Api/AutoFac/DataAccessModule.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using ProductManagment.Api.DataAccess;
+
+namespace ProductManagment.Api.AutoFac
+{
+    public class DataAccessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.Register(c =>
+            {
+                var config = c.Resolve<IConfiguration>();
+
+                var opt = new DbContextOptionsBuilder<DataContext>();
+                opt.UseSqlServer(config.GetConnectionString("Default"));
+
+                return new DataContext(opt.Options);
+            }).AsSelf().InstancePerLifetimeScope();
+        }
+    }
+}
---
+++ ProductManagment/ProductManagment.Api/AutoFac/RequestModule.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using MediatR;
+
+namespace ProductManagment.Api.AutoFac
+{
+    public class RequestModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder
+                .RegisterAssemblyTypes(typeof(IRequest<>).Assembly)
+                .Where(t => t.IsClosedTypeOf(typeof(IRequest<>)))
+                .AsImplementedInterfaces();
+
+            builder
+                .RegisterAssemblyTypes(typeof(IRequestHandler<>).Assembly)
+                .Where(t => t.IsClosedTypeOf(typeof(IRequestHandler<>)))
+                .AsImplementedInterfaces();
+        }
+    }
+}
