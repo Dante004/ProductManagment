1804ef01720ed697f5891fd3f6457f69f9209f0a
Bartosz Bąk <bartosz.bk04@gmail.com>
2020-05-02 19:25:42 +0200

    Added Result

---
+++ ProductManagment/ProductManagment.Api/Helpers/Result.cs
@@ -0,0 +1,63 @@
+﻿using FluentValidation.Results;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProductManagment.Api.Helpers
+{
+    public class Result
+    {
+        public bool Success { get; set; }
+        public IEnumerable<ErrorMessage> Errors { get; set; }
+
+        public static Result<T> Ok<T>(T value)
+        {
+            return new Result<T>()
+            {
+                Success = true,
+                Value = value
+            };
+        }
+
+        public static Result<T> Error<T>(string message)
+        {
+            return new Result<T>
+            {
+                Success = false,
+                Errors = new List<ErrorMessage>()
+                {
+                    new ErrorMessage()
+                    {
+                        PropertyName = string.Empty,
+                        Message = message
+                    }
+                }
+            };
+        }
+
+        public static Result<T> Error<T>(IEnumerable<ValidationFailure> validationFailures)
+        {
+            var result = new Result<T>
+            {
+                Success = false,
+                Errors = validationFailures.Select(v => new ErrorMessage()
+                {
+                    PropertyName = v.PropertyName,
+                    Message = v.ErrorMessage
+                })
+            };
+
+            return result;
+        }
+    }
+
+    public class Result<T> : Result
+    {
+        public T Value;
+    }
+
+    public class ErrorMessage
+    {
+        public string PropertyName { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ ProductManagment/ProductManagment.Api/Helpers/ResultExtension.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace ProductManagment.Api.Helpers
+{
+    public static class ResultExtensions
+    {
+        public static void AddErrorToModelState(this Result result, ModelStateDictionary modelState)
+        {
+            if (result.Success)
+            {
+                return;
+            }
+
+            foreach (var error in result.Errors)
+            {
+                modelState.AddModelError(error.PropertyName, error.Message);
+            }
+        }
+    }
+}
