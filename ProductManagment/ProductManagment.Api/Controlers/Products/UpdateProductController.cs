56a6e65ecec353787cdf6775adcdee9efc673915
Bartosz Bąk <bartosz.bk04@gmail.com>
2020-05-03 11:17:42 +0200

    Changed UpdateProductController to use Result

--- ProductManagment/ProductManagment.Api/Controlers/Products/UpdateProductController.cs
+++ ProductManagment/ProductManagment.Api/Controlers/Products/UpdateProductController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using FluentValidation;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ProductManagment.Api.DataAccess;
+using ProductManagment.Api.Helpers;
 using ProductManagment.Api.Models;
 
 namespace ProductManagment.Api.Controlers.Products
@@ -25,11 +27,18 @@ namespace ProductManagment.Api.Controlers.Products
         [HttpPut]
         public async Task<IActionResult> Put(UpdateProductCommand command, CancellationToken cancellationToken = default)
         {
-            var id = await _mediator.Send(command, cancellationToken);
-            return Ok(id);
+            var result = await _mediator.Send(command, cancellationToken);
+
+            if(!result.Success)
+            {
+                result.AddErrorToModelState(ModelState);
+                return BadRequest(ModelState);
+            }
+
+            return Ok(result);
         }
 
-        public class UpdateProductCommand : IRequest<int>
+        public class UpdateProductCommand : IRequest<Result<int>>
         {
             public int Id { get; set; }
             public string Name { get; set; }
@@ -38,16 +47,19 @@ namespace ProductManagment.Api.Controlers.Products
             public int CategoryId { get; set; }
         }
 
-        public class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommand, int>
+        public class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommand, Result<int>>
         {
-            private DataContext _dataContext;
+            private readonly DataContext _dataContext;
+            private readonly IValidator<Product> _validator;
 
-            public UpdateProductCommandHandler(DataContext dataContext)
+            public UpdateProductCommandHandler(DataContext dataContext,
+                IValidator<Product> validator)
             {
                 _dataContext = dataContext;
+                _validator = validator;
             }
 
-            public async Task<int> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+            public async Task<Result<int>> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
             {
                 var product = new Product
                 {
@@ -61,9 +73,39 @@ namespace ProductManagment.Api.Controlers.Products
                 product.CategoryId = request.CategoryId;
                 product.Price = request.Price;
 
+                var result = await _validator.ValidateAsync(product, cancellationToken);
+
+                if(!result.IsValid)
+                {
+                    return Result.Error<int>(result.Errors);
+                }
+
                 await _dataContext.SaveChangesAsync(cancellationToken);
 
-                return request.Id;
+                return Result.Ok(request.Id);
+            }
+        }
+
+        public class ProductValidtaor : AbstractValidator<Product>
+        {
+            public ProductValidtaor()
+            {
+                RuleFor(p => p.Name)
+                    .Cascade(CascadeMode.StopOnFirstFailure)
+                    .NotEmpty()
+                    .Length(3, 25);
+
+                RuleFor(p => p.Description)
+                    .Length(3, 100);
+
+                RuleFor(p => p.Price)
+                    .Cascade(CascadeMode.StopOnFirstFailure)
+                    .NotEmpty()
+                    .ScalePrecision(2, 5)
+                    .GreaterThanOrEqualTo(0);
+
+                RuleFor(p => p.CategoryId)
+                    .NotNull();
             }
         }
     }
