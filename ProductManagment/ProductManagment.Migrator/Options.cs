7ff2fb529d8d3442c51df0228d6a9c0134b88edb
Bartosz Bąk <bartosz.bk04@gmail.com>
2020-05-05 09:46:26 +0200

    Added MIgrator

---
+++ ProductManagment/ProductManagment.Migrator/Options.cs
@@ -0,0 +1,10 @@
+﻿using CommandLine;
+
+namespace ProductManagment.Migrator
+{
+    public class Options
+    {
+        [Option('c', "connectionString", Required = true, HelpText = "The connection string to database that needs to be updated.")]
+        public string ConnectionString { get; set; }
+    }
+}
---
+++ ProductManagment/ProductManagment.Migrator/Program.cs
@@ -0,0 +1,30 @@
+﻿using CommandLine;
+using Microsoft.EntityFrameworkCore;
+using ProductManagment.Api.DataAccess;
+
+namespace ProductManagment.Migrator
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var result = Parser.Default.ParseArguments<Options>(args);
+
+            result
+                .WithParsed(r => Migrate(r));
+        }
+
+        private static void Migrate(Options options)
+        {
+            var config = new DbContextOptionsBuilder<DataContext>();
+            config
+                .UseSqlServer(options.ConnectionString);
+
+            using (var context = new DataContext(config.Options))
+            {
+                context.Database.Migrate();
+            }
+        }
+    }
+}
