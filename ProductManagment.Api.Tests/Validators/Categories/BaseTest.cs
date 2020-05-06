f5b727dd2dbb07be42d1364fcd9f747af288616a
Bartosz Bąk <bartosz.bk04@gmail.com>
2020-05-06 10:37:38 +0200

    Added valiadotrs tests

---
+++ ProductManagment.Api.Tests/Validators/Categories/BaseTest.cs
@@ -0,0 +1,12 @@
+﻿using static ProductManagment.Api.Controlers.Categories.AddOrUpdateCategoryController;
+
+namespace ProductManagment.Api.Tests.Validators.Categories
+{
+    public class BaseTest
+    {
+        protected virtual CategoryValidtaor Create()
+        {
+            return new CategoryValidtaor();
+        }
+    }
+}
---
+++ ProductManagment.Api.Tests/Validators/Products/BaseTest.cs
@@ -0,0 +1,12 @@
+﻿using static ProductManagment.Api.Controlers.Products.AddOrUpdateProductController;
+
+namespace ProductManagment.Api.Tests.Validators.Products
+{
+    public class BaseTest
+    {
+        protected virtual ProductValidtaor Create()
+        {
+            return new ProductValidtaor();
+        }
+    }
+}
